a4d59454a376e387a1f25529ff12a10b467a7b0c
Caleb Espinoza G <caleb.espinoza@outlook.com>
2021-06-25 00:54:41 -0400

    Add beep

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace beep_windows
+{
+    class Beep {
+        public void ringTheBell () {
+            Console.Beep(2000, 3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beep = new Beep();
+            beep.ringTheBell();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
