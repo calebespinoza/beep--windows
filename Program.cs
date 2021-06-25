142f5d13baa84e6e5680e869da3f0ed91a2e8e8c
Caleb Espinoza G <caleb.espinoza@outlook.com>
2021-06-25 00:58:47 -0400

    Print a Ringing message

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace beep_windows
 {
     class Beep {
         public void ringTheBell () {
+            // Only works on Windows OS
             Console.Beep(2000, 3000);
         }
     }
@@ -12,6 +13,7 @@ namespace beep_windows
         static void Main(string[] args)
         {
             var beep = new Beep();
+            Console.WriteLine("Ringing...");
             beep.ringTheBell();
             //Console.WriteLine("Hello World!");
         }
